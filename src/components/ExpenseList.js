333a271e5a5a60c8b56d281cdbe5771188d9bc0a
Derek Fletes <derekfletes@gmail.com>
2018-08-19 21:58:58 -0700

    Add Expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,16 +4,25 @@ import ExpenseListItem from './ExpenseListItem';
 import getVisibleExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-    <div>
-        {
-            props.expenses.length === 0 ? (
-                <p>No Expenses</p>
-            ) : (
-                    props.expenses.map((expense) => {
-                        return <ExpenseListItem key={expense.id} {...expense} />
-                    })
-                )
-        }
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-amount">Expense</div>
+            <div className="show-for-amount">Amount</div>
+        </div>
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item--message">
+                        <span>No Expenses</span>
+                    </div>
+                ) : (
+                        props.expenses.map((expense) => {
+                            return <ExpenseListItem key={expense.id} {...expense} />
+                        })
+                    )
+            }
+        </div>
     </div>
 );
 
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import SelectExpenses from '../selectors/expenses';
 import SelectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,13 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const formattedExpenseTotal = numeral(expensesTotal / 100).format('$0,0.00');
 
   return (
-    <div>
-      <h1>Viewing {expenseCount} {expenseWord} totaling {formattedExpenseTotal}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totaling <span>{formattedExpenseTotal}</span></h1>
+        <div className="page-header-actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
